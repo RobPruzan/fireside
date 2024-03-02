5189c2a7241e4acb75a1a251dd6997b5c0e6bc28
RobPruzan <rapruzan@buffalo.edu>
2024-03-02 02:38:58 -0500

    fix profile layout

--- services/frontend/src/components/landing/Profile.tsx
+++ services/frontend/src/components/landing/Profile.tsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "@tanstack/react-router";
 
 export const Profile = () => {
-  const { user } = useLoaderData({ from: "/profile" });
+  const { user } = useLoaderData({ from: "/layout/profile" });
 
   return (
     <div className="flex flex-col min-h-calc w-screen items-center justify-start p-10">
