eacc31974d8d0800c74fbc43d77de080b9b3b382
RobPruzan <rapruzan@buffalo.edu>
2024-03-02 20:20:02 -0500

    progress on accepting friend requests

--- services/frontend/src/components/camps/Inbox.tsx
+++ services/frontend/src/components/camps/Inbox.tsx
@@ -1,9 +1,16 @@
+import { FriendRequest } from "@fireside/db";
+import { Button } from "../ui/button";
+import { LoadingSpinner } from "../ui/loading";
 import { useDefinedUser } from "./camps-state";
-import { useGetUserFriendRequests } from "./friends-state";
+import {
+  useAcceptFriendRequestMutation,
+  useGetUserFriendRequests,
+} from "./friends-state";
 
 export const Inbox = () => {
   const { friendRequests } = useGetUserFriendRequests();
   const user = useDefinedUser();
+  const acceptFriendRequestMutation = useAcceptFriendRequestMutation();
   return (
     <div className="h-full w-full flex flex-col">
       {friendRequests.length === 0 && "No friend request"}
@@ -16,8 +23,29 @@ export const Inbox = () => {
           {user.id === friendRequest.fromUserId ? "Me" : friendRequest.toUserId}
           To:{" "}
           {user.id === friendRequest.toUserId ? "Me" : friendRequest.toUserId}
+          <Button
+            onClick={() => {
+              acceptFriendRequestMutation.mutate({
+                requestId: friendRequest.id,
+              });
+            }}
+          >
+            {acceptFriendRequestMutation.isPending ? (
+              <LoadingSpinner />
+            ) : (
+              "Accept Friend Request"
+            )}
+          </Button>
         </div>
       ))}
     </div>
   );
+};
+
+const FriendRequestItem = ({
+  friendRequests,
+}: {
+  friendRequests: FriendRequest;
+}) => {
+  // retur
 };
