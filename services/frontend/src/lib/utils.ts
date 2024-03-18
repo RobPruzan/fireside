d614546bc612fbbde710a39e0b42621bdc8cd310
RobPruzan <rapruzan@buffalo.edu>
2024-03-18 16:55:55 -0400

    Update optimistic updater to be more general

--- services/frontend/src/lib/utils.ts
+++ services/frontend/src/lib/utils.ts
@@ -8,33 +8,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const makeArrayOptimisticUpdater =
-  <
-    TQueryFNResult,
-    TQueryFnResult = InsideArray<InsidePromise<TQueryFNResult>>,
-  >({
-    queryClient,
-    options,
-  }: {
-    queryClient: QueryClient;
-    options: {
-      queryFn: () => TQueryFNResult;
-      queryKey: Array<Nullish<string>>;
-    };
-  }) =>
-  (
-    stateOrUpdater:
-      | Array<TQueryFnResult>
-      | ((prev: Array<TQueryFnResult>) => Array<TQueryFnResult>),
-  ) => {
-    if (typeof stateOrUpdater === "function") {
-      queryClient.setQueryData(options.queryKey, stateOrUpdater);
-      return;
-    }
-
-    queryClient.setQueryData(options.queryKey, () => stateOrUpdater);
-  };
-
 export const makeOptimisticUpdater =
   <TQueryFNResult, TQueryFnResult = InsidePromise<TQueryFNResult>>({
     queryClient,
@@ -47,7 +20,7 @@ export const makeOptimisticUpdater =
     };
   }) =>
   (
-    stateOrUpdater: TQueryFnResult | ((prev: TQueryFnResult) => TQueryFnResult),
+    stateOrUpdater: TQueryFnResult | ((prev: TQueryFnResult) => TQueryFnResult)
   ) => {
     if (typeof stateOrUpdater === "function") {
       queryClient.setQueryData(options.queryKey, stateOrUpdater);
