61e2c53d774fd3d9b7fb19bcfe2b3de760774e87
RobPruzan <rapruzan@buffalo.edu>
2024-03-20 14:45:17 -0400

    hard coded static file serving

--- services/backend/src/index.ts
+++ services/backend/src/index.ts
@@ -6,6 +6,7 @@ import { campRouter } from "./camp-endpoints";
 import { createDB } from "@fireside/db";
 import serverTiming from "@elysiajs/server-timing";
 import { friendRoute } from "./friend-endpoints";
+import staticPlugin from "@elysiajs/static";
 
 const port = 8080;
 
@@ -14,8 +15,7 @@ export const { db } = createDB();
 const authRoutes = new Elysia().use(campRouter).use(userProtectedRoute);
 const noAuthRoutes = new Elysia().use(userRoute);
 
-const app = new Elysia({ prefix: "/api" })
-  .use(serverTiming())
+const app = new Elysia()
   .use(
     cors({
       credentials: true,
@@ -23,11 +23,45 @@ const app = new Elysia({ prefix: "/api" })
       allowedHeaders: ["Origin, X-Requested-With, Content-Type, Accept"],
     })
   )
-  // order matters till v1.0 local scoping can be implemented
-  .use(noAuthRoutes)
-  .use(authRoutes)
-  .use(friendRoute)
-  .get("/", () => Bun.file("public/takodachi.png"))
+  .use(serverTiming())
+  // .use(staticPlugin())
+  // .use(
+  //   staticPlugin({
+  //     prefix: "static",
+  //   })
+  // )
+  .group("/api", (app) =>
+    app
+
+      // order matters till v1.0 local scoping can be implemented
+      .use(noAuthRoutes)
+      .use(authRoutes)
+      .use(friendRoute)
+  )
+  // .get("/", () => Bun.file("static/index.html"))
+  .use(staticPlugin({ prefix: "/", assets: "./public" }))
+  .get("/*", async ({ path }) => {
+    const assetFile = Bun.file(
+      `./static/assets/${path.replaceAll("/", "").replace("assets", "")}`
+    );
+    const publicFile = Bun.file(`./public/${path.replaceAll("/", "")}`);
+    const fallBackFile = Bun.file("./static/index.html");
+    console.log(
+      path,
+      await assetFile.exists(),
+      await publicFile.exists(),
+      await fallBackFile.exists()
+    );
+    if (await assetFile.exists()) {
+      return assetFile;
+    }
+
+    if (await publicFile.exists()) {
+      return publicFile;
+    }
+
+    return fallBackFile;
+  })
 
   .onError(({ error }) => {
     return error.toString();
@@ -37,3 +71,4 @@ const app = new Elysia({ prefix: "/api" })
 
 console.log(`Running on port ${port}`);
 export type App = typeof app;
+//
