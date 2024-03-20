4c8d19d7b75cdc97f2fe3d49386180df3e5a7eb0
RobPruzan <rapruzan@buffalo.edu>
2024-03-20 16:48:13 -0400

    fix error handling

--- services/frontend/src/edenClient.ts
+++ services/frontend/src/edenClient.ts
@@ -2,6 +2,7 @@ import { treaty } from "@elysiajs/eden";
 
 import { App } from "@fireside/backend";
 import { Nullish } from "@fireside/utils";
+import { z } from "zod";
 // @ts-expect-error  types are being wonky for some reason
 export const client = treaty<App>(import.meta.env.VITE_API_URL, {
   fetch: {
@@ -14,8 +15,24 @@ type SimpleResponse<T> = {
   data: Nullish<T>;
 };
 export const dataOrThrow = <T>(response: SimpleResponse<T>) => {
+  // i don't like this error handling but whatever
+
   if (response.error?.value) {
-    throw new Error(JSON.stringify(response.error.value));
+    const isStringRes = z.string().safeParse(response.error.value);
+
+    if (isStringRes.success) {
+      const hasMessageRes = z
+        .object({ message: z.string() })
+        .safeParse(JSON.parse(isStringRes.data));
+
+      if (hasMessageRes.success) {
+        throw new Error(hasMessageRes.data.message);
+      }
+
+      throw new Error(isStringRes.data);
+    }
+    console.error(JSON.stringify(response.error));
+    throw new Error("Unknown error");
   }
 
   return response.data as T;
