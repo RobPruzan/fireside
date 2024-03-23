9fe43d0dd0885fd72049148c5bd2bab3c7dac0a0
RobPruzan <rapruzan@buffalo.edu>
2024-03-22 23:44:04 -0400

    ui for reactions

--- services/frontend/src/components/camps/Camp.tsx
+++ services/frontend/src/components/camps/Camp.tsx
@@ -2,8 +2,30 @@ import { useParams } from "@tanstack/react-router";
 import { Input } from "../ui/input";
 import { useEffect, useRef, useState } from "react";
 import { useCreateMessageMutation, useGetMessages } from "./camps-state";
-import { client } from "@/edenClient";
-
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import { cn } from "@/lib/utils";
+import {
+  ArrowDown,
+  ArrowUp,
+  Edit,
+  Flame,
+  Frown,
+  Skull,
+  Smile,
+  SmilePlus,
+  Trash,
+} from "lucide-react";
+import { Button } from "../ui/button";
 export const Camp = () => {
   const [userMessage, setUserMessage] = useState<string>("");
   const { campId } = useParams({ from: "/root-auth/camp-layout/camp/$campId" });
@@ -27,44 +49,18 @@ export const Camp = () => {
     }
   }, [messages]);
 
-  //   const isInViewport = (element) => {
-  //     const rect = element.getBoundingClientRect();
-  //     return (
-  //         rect.top >= 0 &&
-  //         rect.left >= 0 &&
-  //         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-  //         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  //     );
-  // }
-
   const createMessageMutation = useCreateMessageMutation({ campId });
 
-  const handleLike = async (messageId: string) => {
-    const response = await client.api.protected.camp.message.like.post({
-      campId,
-      messageId,
-    });
+  const [messageWithContextMenuId, setMessageWithContextMenuId] = useState<
+    null | string
+  >(null);
 
-    if (response.data !== null && "totalLikes" in response.data) {
-      const { totalLikes } = response.data;
-      setLikesCounts((prevLikesCounts) => ({
-        ...prevLikesCounts,
-        [messageId]: totalLikes,
-      }));
-    } else {
-      setLikesCounts((prevLikesCounts) => ({
-        ...prevLikesCounts,
-        [messageId]: 0,
-      }));
-      console.error("Invalid response format");
-    }
-  };
-
+  console.log("yay", { messageWithContextMenuId });
   return (
     <div className="flex flex-col w-full h-full p-5 gap-y-4">
       <div
         ref={scrollRef}
-        className="flex flex-col w-full h-[calc(100%-50px)] overflow-auto gap-y-5"
+        className="flex flex-col w-full h-[calc(100%-50px)] overflow-auto gap-y-6"
       >
         {messages
           .sort(
@@ -72,11 +68,92 @@ export const Camp = () => {
               new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
           )
           .map((messageObj) => (
-            <div className="flex">
+            <div className="flex items-center gap-x-2">
               {messageObj.user.email}
-              <div className="p-4 border-l-2" key={messageObj.id}>
-                {messageObj.message}
-              </div>
+              <ContextMenu
+                onOpenChange={(v) => {
+                  if (v) {
+                    setMessageWithContextMenuId(messageObj.id);
+                    return;
+                  }
+
+                  setMessageWithContextMenuId(null);
+                }}
+              >
+                <ContextMenuTrigger asChild>
+                  <div
+                    className={cn([
+                      "w-full flex hover:bg-muted",
+                      messageWithContextMenuId === messageObj.id && "bg-muted",
+                    ])}
+                  >
+                    <div className="p-4 border-l-2 w-3/4" key={messageObj.id}>
+                      {messageObj.message}
+                    </div>
+                    <div className="w-1/4 flex justify-end items-center">
+                      <span className="text-sm">
+                        {new Date(messageObj.createdAt).toLocaleTimeString()}
+                      </span>
+                    </div>
+                  </div>
+                </ContextMenuTrigger>
+                <ContextMenuContent>
+                  <ContextMenuItem className="flex gap-x-2">
+                    <Edit size={17} /> Edit
+                  </ContextMenuItem>
+
+                  <ContextMenuSub>
+                    <ContextMenuSubTrigger className="flex gap-x-2">
+                      <SmilePlus size={17} /> Reactions
+                    </ContextMenuSubTrigger>
+                    <ContextMenuSubContent className="w-48 flex flex-wrap  justify-evenly items-center">
+                      {/* <div className="w-full">
+                        <Input />
+                      </div> */}
+
+                      <Button
+                        className="flex items-center w-fit h-fit justify-center"
+                        variant={"ghost"}
+                      >
+                        <Smile size={17} />
+                      </Button>
+                      <Button
+                        className="flex items-center w-fit h-fit justify-center"
+                        variant={"ghost"}
+                      >
+                        <Frown size={17} />
+                      </Button>
+                      <Button
+                        className="flex items-center w-fit h-fit justify-center"
+                        variant={"ghost"}
+                      >
+                        <ArrowUp size={17} />
+                      </Button>
+                      <Button
+                        className="flex items-center w-fit h-fit justify-center"
+                        variant={"ghost"}
+                      >
+                        <ArrowDown size={17} />
+                      </Button>
+                      <Button
+                        className="flex items-center w-fit h-fit justify-center"
+                        variant={"ghost"}
+                      >
+                        <Skull size={17} />
+                      </Button>
+                      <Button
+                        className="flex items-center w-fit h-fit justify-center"
+                        variant={"ghost"}
+                      >
+                        <Flame size={17} />
+                      </Button>
+                    </ContextMenuSubContent>
+                  </ContextMenuSub>
+                  <ContextMenuItem className="flex gap-x-2 text-destructive">
+                    <Trash size={17} /> Delete
+                  </ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
             </div>
           ))}
       </div>
