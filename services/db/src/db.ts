fd5099303b3ae48ca340ba8f01bda2b73bdc76a9
dgage200 <123893231+dgage200@users.noreply.github.com>
2024-03-24 14:43:59 -0400

    Checked db.ts hardcoded connection

--- services/db/src/db.ts
+++ services/db/src/db.ts
@@ -7,6 +7,6 @@ config({ path: join("..", ".env") });
 config({ path: join(".env") });
 export const createDB = () => {
   console.log("conn string we got", process.env.CONNECTION_STRING);
-  const sql = postgres(process.env.CONNECTION_STRING!, { max: 1 });
+  const sql = postgres("postgres://test_user:test_password@localhost:5432/test_db", { max: 1 });
   return { db: drizzle(sql), sql };
 };
