6039b7e504494c30c7f4e1a07bddf5d65961d6bc
RobPruzan <rapruzan@buffalo.edu>
2024-03-25 00:02:09 -0400

    make sure reading env in db relative to dir of file

--- services/db/src/db.ts
+++ services/db/src/db.ts
@@ -3,8 +3,9 @@ import postgres from "postgres";
 import { config } from "dotenv";
 import { join } from "path";
 // yolo
-config({ path: join("..", ".env") });
-config({ path: join(".env") });
+config({ path: join(__dirname, "..", ".env") });
+config({ path: join(__dirname, ".env") });
+
 export const createDB = () => {
   console.log("conn string we got", process.env.CONNECTION_STRING);
   const sql = postgres(process.env.CONNECTION_STRING!, { max: 1 });
