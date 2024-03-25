9902fae0e2ed553c8daa34c41646d8146c010960
RobPruzan <rapruzan@buffalo.edu>
2024-03-25 01:07:25 -0400

    sort thread messages on time

--- services/frontend/src/components/camps/Thread.tsx
+++ services/frontend/src/components/camps/Thread.tsx
@@ -69,32 +69,38 @@ export const Thread = () => {
         ref={scrollRef}
         className="p-2 flex flex-col h-[calc(100%-85px)] gap-y-2 w-full overflow-y-scroll"
       >
-        {threadMessages.map((threadMessage, index) => (
-          <div
-            className={cn([
-              " w-fit p-2 flex items-start flex-col",
-              threadMessage.userId === user.id ? "ml-auto" : "mr-auto",
-              index === threadMessages.length - 1 && "pb-4",
-              index === 0 && "pt-4",
-            ])}
-          >
-            {threadMessage.user.email}
-            <div className="flex">
-              <div
-                className={cn([
-                  "border rounded-md  p-2",
-                  threadMessage.userId === user.id && "bg-primary",
-                ])}
-              >
-                {threadMessage.message}
-              </div>
+        {[...threadMessages]
+          .sort(
+            (a, b) =>
+              new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+          )
 
-              {/* <div className="text-xs flex items-center">
+          .map((threadMessage, index) => (
+            <div
+              className={cn([
+                " w-fit p-2 flex items-start flex-col",
+                threadMessage.userId === user.id ? "ml-auto" : "mr-auto",
+                index === threadMessages.length - 1 && "pb-4",
+                index === 0 && "pt-4",
+              ])}
+            >
+              {threadMessage.user.email}
+              <div className="flex">
+                <div
+                  className={cn([
+                    "border rounded-md  p-2",
+                    threadMessage.userId === user.id && "bg-primary",
+                  ])}
+                >
+                  {threadMessage.message}
+                </div>
+
+                {/* <div className="text-xs flex items-center">
                 {new Date(threadMessage.createdAt).toLocaleTimeString()}
               </div> */}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       <Textarea
         onChange={(e) => setCurrentMessage(e.target.value)}
