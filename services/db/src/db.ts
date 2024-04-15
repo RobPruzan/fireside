4edf08336c32a2f63ad0aaaa8e7c6e64ed90e9d7
RobPruzan <rapruzan@buffalo.edu>
2024-04-15 14:28:16 -0400

    remove max conns arguemnt, setup logs

--- services/db/src/db.ts
+++ services/db/src/db.ts
@@ -8,6 +8,16 @@ config({ path: join(__dirname, ".env") });
 
 export const createDB = ({ connString }: { connString: string }) => {
   console.log(connString);
-  const sql = postgres(connString, { max: 1 });
+  const sql = postgres(connString, {
+    onnotice: (notice) => {
+      console.log("PG NOTICE", notice);
+    },
+    onclose: (connId) => {
+      console.log("PG CLOSE", config);
+    },
+    onparameter: (key, value) => {
+      console.log("PARAM CHANGE", key, value);
+    },
+  });
   return { db: drizzle(sql), sql };
 };
